75052be08bd16b32135f7befb3c23acbee6dfa51
mouain02 <moueynaloui19@gmail.com>
2026-07-19 18:00:37 +0100

    store fix

---
+++ server/config/storeConfig.js
@@ -0,0 +1,23 @@
+export default {
+    currency: "TND",
+  
+    shipping: {
+      flatRate: 15,
+      freeThreshold: 200,
+    },
+  
+    lensPricing: {
+      types: {
+        single: 0,
+        progressive: 150,
+        bifocal: 100,
+      },
+  
+      treatments: {
+        "anti-reflective": 50,
+        "blue-light": 80,
+        uv: 30,
+        photochromic: 120,
+      },
+    },
+  };
