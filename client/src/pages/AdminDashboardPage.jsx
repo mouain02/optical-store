115849268cd19c8da0ba6eb24431adad997b2fc2
mouain02 <moueynaloui19@gmail.com>
2026-07-25 20:44:31 +0100

    admindash fix

--- client/src/pages/AdminDashboardPage.jsx
+++ client/src/pages/AdminDashboardPage.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 import useAdminDashboard from "../hooks/useAdminDashboard";
 
-import OverviewTab from "./tabs/OverviewTab";
-import UsersTab from "./tabs/UsersTab";
-import ProductsTab from "./tabs/ProductsTab";
-import OrdersTab from "./tabs/OrdersTab";
-import ReviewsTab from "./tabs/ReviewsTab";
-import CouponsTab from "./tabs/CouponsTab";
-import BrandsTab from "./tabs/BrandsTab";
+import OverviewTab from "./admin/tabs/OverviewTab";
+import UsersTab from "./admin/tabs/UsersTab";
+import ProductsTab from "./admin/tabs/ProductsTab";
+import OrdersTab from "./admin/tabs/OrdersTab";
+import ReviewsTab from "./admin/tabs/ReviewsTab";
+import CouponsTab from "./admin/tabs/CouponsTab";
+import BrandsTab from "./admin/tabs/BrandsTab";
 
 import Loader from "../../components/common/Loader";
 
